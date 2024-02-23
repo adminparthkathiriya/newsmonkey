4d2ce7332161090c5763f17406788fa7bfa0d9d1
adminparthkathiriya <141150712+adminparthkathiriya@users.noreply.github.com>
2024-02-23 16:56:48 +0530

    add

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            NewsMonkey
+            NewsMonkey Add
           </Link>
           <button
             className="navbar-toggler"
